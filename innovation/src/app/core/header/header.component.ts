bc66bdc248515e73feec8f106e848f43e518ee7d
tjorven - HP-9K5T3EA <burdorftjorven@gmail.com>
2025-05-08 20:53:59 +0200

    add header content

--- innovation/src/app/core/header/header.component.ts
+++ innovation/src/app/core/header/header.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    imports: [
+        RouterLink,
+        NgIf
+    ],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+    constructor() {
+    }
 
+    protected readonly document = document;
 }
