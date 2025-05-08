d7198b8f25987ec27e8f375bbbcc122fdf60ab4f
tjorven - HP-9K5T3EA <burdorftjorven@gmail.com>
2025-05-08 20:29:44 +0200

    add base components

--- innovation/src/app/app.routes.ts
+++ innovation/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import {Routes} from '@angular/router';
+import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
+import {MainComponent} from './features/main/main.component';
+import {HomeComponent} from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'main', component: MainComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
