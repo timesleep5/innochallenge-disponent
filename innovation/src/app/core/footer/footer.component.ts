0b57f1a0c3c4b76e9eefe1ee672b301bd7b2f61f
Toba4711 <bastian.hoeschl@gmx.net>
2025-05-08 23:11:52 +0200

    design header and footer

--- innovation/src/app/core/footer/footer.component.ts
+++ innovation/src/app/core/footer/footer.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+    selector: 'app-footer',
+    imports: [RouterLink],
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
