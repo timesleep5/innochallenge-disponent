727c95d2c0c1673bc8e9c7e9c1d7df378eccac24
tjorven - HP-9K5T3EA <burdorftjorven@gmail.com>
2025-05-09 05:22:37 +0200

    create CREATOR page

--- innovation/src/app/app.routes.ts
+++ innovation/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import {Routes} from '@angular/router';
 import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
 import {MainComponent} from './features/main/main.component';
 import {HomeComponent} from './features/home/home.component';
-import { LeafletTestComponent } from './features/leaflet-test/leaflet-test.component';
+import {LeafletTestComponent} from './features/leaflet-test/leaflet-test.component';
+import {CreatorComponent} from './features/creator/creator.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'main', component: MainComponent},
-    {path: "leaflet", component: LeafletTestComponent},
+    {path: 'creator', component: CreatorComponent},
+    {path: 'leaflet', component: LeafletTestComponent},
     {path: '**', component: PageNotFoundComponent}
 ];
